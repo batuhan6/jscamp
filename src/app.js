ebeb721e82e1f8d3fdf54849a7afc45b7acb3efb
batuhan6 <bba@grass-merkur.de>
2021-12-16 14:45:41 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+// console.log("Merhaba kodlama.io")
+
+// let dolarDun = 5
+
+// {
+//     let dolarDun = 9.10   //tanimlandiklari blokta gecerlidir.  var da olsaydi degisecekti ondan var kullanmiyoruz artik
+// }
+// console.log(dolarDun)
+
+// const euroDun = 11
+// console.log(euroDun)
+
+let konutKredileri = [12,"b",["w","x","y"]]
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>" + konutKredileri[i] + "</li>")    
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
